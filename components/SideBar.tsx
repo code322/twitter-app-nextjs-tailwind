3d646562c44a2b9794fddd4bd79ba3b2ebb52e96
Tcodingg <princetlive@yahoo.com>
2022-06-19 17:52:29 -0400

    create sideBarorw reusable component and add all the sidebar items. add a new color to twitter config

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
-
+import {
+	BellIcon,
+	HashtagIcon,
+	BookmarkIcon,
+	CollectionIcon,
+	DotsCircleHorizontalIcon,
+	MailIcon,
+	UserIcon,
+	HomeIcon,
+} from '@heroicons/react/outline';
+import SideBarRow from './SideBarRow';
 function SideBar() {
-	return <div>SideBar</div>;
+	return (
+		<div>
+			<img className='h-10 w-10' src='https://links.papareact.com/drg' alt='' />
+
+			<SideBarRow Icon={HomeIcon} title='home' />
+			<SideBarRow Icon={HashtagIcon} title='Explore' />
+			<SideBarRow Icon={BellIcon} title='Notification' />
+			<SideBarRow Icon={MailIcon} title='Messages' />
+			<SideBarRow Icon={BookmarkIcon} title='Bookmarks' />
+			<SideBarRow Icon={CollectionIcon} title='Lists' />
+		</div>
+	);
 }
 
 export default SideBar;
