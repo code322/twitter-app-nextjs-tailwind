5d9a27371b007b1b83855c7ea8337cfdf7b039ba
Tcodingg <princetlive@yahoo.com>
2022-07-07 23:56:58 -0400

    add more icons

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -13,7 +13,11 @@ import SideBarRow from './SideBarRow';
 function SideBar() {
 	return (
 		<div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
-			<img className='m-3 h-10 w-10' src='https://links.papareact.com/drg' alt='' />
+			<img
+				className='m-3 h-10 w-10'
+				src='https://links.papareact.com/drg'
+				alt=''
+			/>
 			<SideBarRow Icon={HomeIcon} title='home' />
 			<SideBarRow Icon={HashtagIcon} title='Explore' />
 			<SideBarRow Icon={BellIcon} title='Notification' />
@@ -21,6 +25,8 @@ function SideBar() {
 			<SideBarRow Icon={BookmarkIcon} title='Bookmarks' />
 			<SideBarRow Icon={CollectionIcon} title='Lists' />
 			<SideBarRow Icon={UserIcon} title='Sign In' />
+			<SideBarRow Icon={DotsCircleHorizontalIcon} title='More' />
+			<SideBarRow Icon={DotsCircleHorizontalIcon} title='More' />
 			<SideBarRow Icon={DotsCircleHorizontalIcon} title='More' />
 		</div>
 	);
