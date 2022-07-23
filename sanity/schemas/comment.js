268aaa78ba5f8c875dbcce849557ad7847865609
Tcodingg <princetlive@yahoo.com>
2022-07-23 15:48:23 -0400

    modify the schema and move the .babelrc to sanity folder

--- sanity/schemas/comment.js
+++ sanity/schemas/comment.js
@@ -1,48 +1,31 @@
 export default {
-  name: 'author',
-  title: 'Author',
-  type: 'document',
-  fields: [
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96,
-      },
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
-        {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        },
-      ],
-    },
-  ],
-  preview: {
-    select: {
-      title: 'name',
-      media: 'image',
-    },
-  },
-}
+	name: 'comment',
+	title: 'Comment',
+	type: 'document',
+	fields: [
+		{
+			name: 'comment',
+			title: 'comment',
+			type: 'string',
+		},
+		{
+			name: 'username',
+			title: 'Username',
+			type: 'string',
+		},
+		{
+			name: 'profileImg',
+			title: 'Profile Image',
+			type: 'string',
+		},
+		{
+			name: 'tweet',
+			title: 'Tweet',
+			description: 'Reference the Tweet the comment is associated to:',
+			type: 'reference',
+			to: {
+				type: 'tweet',
+			},
+		},
+	],
+};
--- sanity/schemas/tweet.js
+++ sanity/schemas/tweet.js
@@ -1,5 +1,5 @@
 export default {
-	name: 'Tweet',
+	name: 'tweet',
 	title: 'Tweet',
 	type: 'document',
 	fields: [
