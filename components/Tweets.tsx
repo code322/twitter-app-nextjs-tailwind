9e0d8f7bed6b145543c7f8a9be10ebc735133e68
Tcodingg <princetlive@yahoo.com>
2022-07-24 11:38:14 -0400

    install timeago dependecy. add timeago for all tweets

--- components/Tweets.tsx
+++ components/Tweets.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Tweet } from '../typings';
-
+import TimeAgo from 'react-timeago';
 interface Props {
 	tweet: Tweet;
 }
+
 const Tweets = ({ tweet }: Props) => {
 	return (
 		<div>
@@ -11,7 +12,10 @@ const Tweets = ({ tweet }: Props) => {
 				<img src={tweet.profileImage} alt='' />
 				<div>
 					<p>{tweet.username}</p>
-					<p></p>
+					<p className='hidden text-sm text-gray-500'>
+						@{tweet.username.replace(/\s+/g, '').toLocaleLowerCase()}
+					</p>
+					<TimeAgo className='text-sm text-gray-500' date={tweet._createdAt} />
 				</div>
 			</div>
 		</div>
