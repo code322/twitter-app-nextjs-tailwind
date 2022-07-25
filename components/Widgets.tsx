2d4fab192ea2b188df3d49fafc1f60ae702989e0
Tcodingg <princetlive@yahoo.com>
2022-07-25 12:47:02 -0400

    style and wrap the search icon

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,26 +1,25 @@
 import { SearchIcon } from '@heroicons/react/outline';
 import React from 'react';
-import { TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
-
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 function Widgets() {
 	return (
 		<div className='mt-2 px-2 col-span-2 hidden lg:inline'>
 			{/* Search */}
 			<div className='flex items-center space-x-2 bg-gray-100 p-3'>
-				<SearchIcon className='h-5 w-5 text-gray-400' />
+				<div>
+					<SearchIcon className='h-5 w-5 text-gray-400' />
+				</div>
 				<input
 					type='text'
 					placeholder='Search Twitter'
 					className='bg-transparent flex-1 outline-none'
 				/>
 			</div>
-			<div>
-				<TwitterTimelineEmbed
-					sourceType='profile'
-					screenName='nextjs'
-					options={{ height: 400 }}
-				/>
-			</div>
+			<TwitterTimelineEmbed
+				sourceType='profile'
+				screenName='nextjs'
+				options={{ height: 1000 }}
+			/>
 		</div>
 	);
 }
