6373755645d784832ab0c5819cd424a78c3244ec
Tcodingg <princetlive@yahoo.com>
2022-07-26 11:53:11 -0400

    Add toast dependecy. When the refresh button is clicked Refresh the feed and through a toast

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,18 +1,32 @@
 import { RefreshIcon } from '@heroicons/react/outline';
-import React from 'react';
+import React, { useState } from 'react';
 import { Tweet } from '../typings';
+import { fetchTweets } from '../utils/fetchTweets';
 import TweetBox from './TweetBox';
 import TweetComponent from './Tweets';
-
+import toast, { Toaster } from 'react-hot-toast';
 interface Props {
 	tweets: Tweet[];
 }
-function Feed({ tweets }: Props) {
+function Feed({ tweets: tweetsProp }: Props) {
+	const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
+	const handleRefresh = async () => {
+		const refreshToast = toast.loading('refreshing...');
+		const tweets = await fetchTweets();
+		setTweets(tweets);
+		toast.success('Feed updated', {
+			id: refreshToast,
+		});
+	};
+
 	return (
 		<div className='col-span-7 lg:col-span-5 border-x'>
 			<div className='flex items-center justify-between'>
 				<h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
-				<RefreshIcon className='mr-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125' />
+				<RefreshIcon
+					onClick={handleRefresh}
+					className='mr-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125'
+				/>
 			</div>
 
 			<div>
@@ -21,10 +35,11 @@ function Feed({ tweets }: Props) {
 
 			<div>
 				{/* Feed */}
-				{tweets.map((tweet) => (
+				{tweetsProp.map((tweet) => (
 					<TweetComponent key={tweet._id} tweet={tweet} />
 				))}
 			</div>
+			<Toaster />
 		</div>
 	);
 }
--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,10 +1,12 @@
 import { Tweet } from '../typings';
 const { URL } = process.env;
 export const fetchTweets = async () => {
+	let URL = 'http://localhost:3000';
 	const res = await fetch(URL + '/api/getTweets');
 
 	const data = await res.json();
 	const tweets: Tweet[] = data.tweets;
+	console.log(URL, tweets);
 
 	return tweets;
 };
